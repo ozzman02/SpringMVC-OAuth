70ca87776004ccd050cac2c990dde73fa18801e9
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-11-24 19:57:56 -0600

    adding test cases

---
+++ projects/4. Test/4.1TestWebContext/Final/web/src/test/java/lab/spring/io/web/test/CustomerControllerTest.java
@@ -0,0 +1,53 @@
+package lab.spring.io.web.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lab.spring.io.config.CustomerWebContextConfig;
+import lab.spring.io.config.RootContextConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {CustomerWebContextConfig.class, RootContextConfig.class})
+@WebAppConfiguration
+public class CustomerControllerTest {
+	
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mockMvc; 
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		
+	}
+	
+	@Test
+	public void testAppContext() throws UnsupportedEncodingException, Exception {
+		String expected = "Welcome home";
+		String actual = mockMvc.perform(MockMvcRequestBuilders.get("/")).andReturn().getResponse().getContentAsString();
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testWelcomeURI() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/welcome/2"))
+													.andExpect(MockMvcResultMatchers.view().name("welcome"))
+													.andExpect(MockMvcResultMatchers.model().attribute("name", "INAM"));
+	}
+
+}
